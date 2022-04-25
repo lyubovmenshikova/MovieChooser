418c62b2150b1c3a382bcfdc7d96c653adda9f73
lyubovmenshikova <dyachenko.lubov@yandex.ru>
2022-04-25 23:36:07 +0300

    add actorList

--- MovieChooser/MovieChooser/Actor View Controllers/ActorSearchViewController.swift
+++ MovieChooser/MovieChooser/Actor View Controllers/ActorSearchViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ActorSearchViewController: UIViewController {
     
+    @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var searchButton: UIButton!
+    
+    var mainColor = UIColor(red: 17/255, green: 188/255, blue: 214/255, alpha: 1)
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .darkContent
     }
@@ -27,6 +32,34 @@ class ActorSearchViewController: UIViewController {
     private func setupAppearance() {
         navigationItem.largeTitleDisplayMode = .never
         view.backgroundColor = .secondarySystemBackground
+        
+        navigationController?.navigationBar.tintColor = mainColor
+        
+        searchButton.tintColor = mainColor
+        searchButton.layer.borderColor = mainColor.cgColor
+        searchButton.layer.borderWidth = 1
+        searchButton.layer.cornerRadius = 10
+        
+        searchTextField.placeholder = "Введите имя актера"
     }
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        guard let name = searchTextField.text else { return }
+        if name.isEmpty {
+            showAlertController(with: "Ошибка", message: "Заполните поле")
+            return
+        } else {
+            performSegue(withIdentifier: "toActorFilm", sender: nil)
+        }
+    }
+    
+    private func showAlertController(with title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "ОК", style: .default)
+        alertController.addAction(okAction)
+        present(alertController, animated: true)
+    }
+    
 
 }
