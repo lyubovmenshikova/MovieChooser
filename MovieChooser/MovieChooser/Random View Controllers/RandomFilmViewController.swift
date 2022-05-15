7abd2e42e5250bf352208efcae55c64e22e3de24
lyubovmenshikova <dyachenko.lubov@yandex.ru>
2022-05-15 23:54:59 +0300

    add logic to random VC

--- MovieChooser/MovieChooser/Random View Controllers/RandomFilmViewController.swift
+++ MovieChooser/MovieChooser/Random View Controllers/RandomFilmViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class RandomFilmViewController: UIViewController {
     
+    @IBOutlet var filmImageView: UIImageView!
+    @IBOutlet var filmTitleLabel: UILabel!
+    @IBOutlet var generationButton: UIButton!
+    
     var mainColor = UIColor(red: 208/255, green: 224/255, blue: 56/255, alpha: 1)
 
     override func viewDidLoad() {
@@ -22,9 +26,17 @@ class RandomFilmViewController: UIViewController {
         view.backgroundColor = .secondarySystemBackground
         
         navigationController?.navigationBar.tintColor = mainColor
+        
+        generationButton.backgroundColor = mainColor
+        generationButton.tintColor = .white
+        generationButton.layer.cornerRadius = 10
+        
+        filmTitleLabel.font = UIFont(name: "TrebuchetMS", size: 16)
     }
     
-
+    @IBAction func generationButtonPressed(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
